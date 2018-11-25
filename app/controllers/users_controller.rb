0a79a742d319d1288c5b24c37c41fad1f18f2932
mehirpandit <mp91117@uga.edu>
2018-11-25 15:31:49 -0500

    now reset maybe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
 
   def index
 
-       @users = User.paginate(page: params[:page])
 
+       @users = User.paginate(page: params[:page])
   end
 
   def show
